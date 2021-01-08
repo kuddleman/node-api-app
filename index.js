d367718df7c971fa59c2f43b3b3bc08de27891f5
Kuddleman <edl1155@gmail.com>
2021-01-07 22:00:25 -0800

    add post with joi error validation

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
+
+// This is middleware
+app.use(express.json())
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -12,11 +16,41 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/api/courses', (req, res)=>{
-    res.send([1,2,3])
+    res.send(courses)
 })
 
 app.get('/api/courses/:id',( req, res )=>{
-    res.send(req.params.id)
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send('The course with the given id was not found')
+    res.send(course)
+})
+
+// Post request to create a new course
+app.post('/api/courses', (req, res)=>{
+
+     //validation using Joi package
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    const result = schema.validate(req.body)
+    
+   
+    if (result.error) {
+        // 400 bad request
+        res.status(400).send(result.error.details[0].message)
+        return
+    }
+
+// create new course object
+    const course = {
+        id: courses.length + 1,
+        // read the name from the body of the request
+        name: req.body.name
+    }
+    courses.push(course)
+    // when the server creates a new object, it should return the new 
+    // resource in the body of the response. 
+    res.send(course)
 })
 
 //Port
